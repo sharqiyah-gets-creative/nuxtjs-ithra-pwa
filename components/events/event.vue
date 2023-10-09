888c840553fc3ac0ac23111e305c273334a9cae9
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-10-09 17:28:51 +0300

    chore: restructure events

---
+++ components/events/event.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useCommonStore } from '~/store/common';
+const { lastColor, setLastColor } = useCommonStore();
+
+let color = getEventHeaderClass();
+
+// if the generated color is the same as the previous one in the store, generate another
+while (color === lastColor) {
+  color = getEventHeaderClass();
+}
+
+setLastColor(color);
+
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  timing: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div class="w-full text-slate-900 rounded-xl overflow-hidden shadow-md">
+    <div :class="`${color}`" class="p-2 px-4 text-lg font-bold">{{ name }}</div>
+    <div class="p-2 px-4 bg-white">
+      <ul class="space-y-1 font-light text-base">
+        <li class="flex justify-start items-center">
+          <UIcon class="me-2" name="i-heroicons-clock" />{{ timing }}
+        </li>
+        <li class="flex justify-start items-center">
+          <UIcon class="me-2" name="i-heroicons-map-pin-solid" />{{ location }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
---
+++ components/events/section.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+
+let eventsArray: any = ref([]);
+
+onMounted(async () => {
+    const nuxtApp = useNuxtApp();
+    const config = nuxtApp.$config;
+    const { events } = await getEvents(config);
+    
+    events.forEach((event) => {
+        eventsArray.value.push(event);
+    });
+    
+    console.log(eventsArray)
+});
+
+</script>
+
+<template>
+    <section id="events" class="py-4">
+        <UContainer class="space-y-8">
+            <eventsEvent 
+            v-if="eventsArray.length > 0"
+            v-for="event in eventsArray"
+            :key="event.id" 
+            :name="event.title" 
+            :location="event.location"
+            :timing="event.timing" 
+            />
+
+            <div v-else class="text-center">
+                <UCard loading :loading-state="{ icon: 'i-heroicons-arrow-path-20-solid', label: 'Loading...' }">
+                    <h2 class="text-3xl font-black">جاري جلب الفعاليات</h2>
+                </UCard> 
+            </div>
+        </UContainer>
+    </section>
+</template>
