d9e944d887d3f2f0ea84f4f4f77f96676c24aae0
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-10-09 21:24:27 +0300

    chore: modularize bottomNav Links

---
+++ components/navs/bottomNavLink.vue
@@ -0,0 +1,25 @@
+<template>
+  <NuxtLink
+        :to="link"
+        class="
+        pt-2 px-5 standalone:pb-6 
+        inline-flex flex-col items-center justify-center 
+        text-slate-800
+        text:bg-purple-300 
+
+        dark:text-gray-200 
+        dark:hocus:bg-purple-900
+        transition-colors duration-300
+        "
+      >
+        <UIcon class="text-3xl" :name="icon" />
+        <span class="text-sm">{{ title }}</span>
+      </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+defineProps(["title", "icon", "link"])
+
+</script>
+
+
--- components/navs/flowbiteNav.vue
+++ components/navs/flowbiteNav.vue
@@ -1,31 +1,29 @@
 <template>
   <div
-    class=" pt-2 sticky bottom-0 z-50 w-full  bg-white border-t border-gray-200 dark:bg-primary-violet dark:border-gray-600"
+    class="sticky bottom-0 z-50 w-full bg-white border-t border-gray-200 dark:bg-primary-violet dark:border-gray-600"
   >
     <div class="grid h-full max-w-lg grid-cols-3 mx-auto font-medium">
-      <button
-        type="button"
-        class="standalone:pb-6 inline-flex text-gray-500 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-500 flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-      >
-        <UIcon class="text-3xl" name="i-mdi-home" />
-        <span class="text-sm">الرئيسية</span>
-      </button>
 
-      <button
-        type="button"
-        class="standalone:pb-6 inline-flex text-gray-500 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-500 flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-      >
-        <UIcon class="text-3xl" name="i-mdi-calendar" />
-        <span class="text-sm">المبادرات</span>
-      </button>
+      <NavsBottomNavLink
+        title="المبادرات"
+        icon="i-mdi-home"
+        link="/"
+      />
 
-      <button
-        type="button"
-        class="standalone:pb-6 inline-flex text-gray-500 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-500 flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-      >
-        <UIcon class="text-3xl" name="i-mdi-help-circle" />
-        <span class="text-sm">نبذة</span>
-      </button>
+      <NavsBottomNavLink
+        title="الروزنامة"
+        icon="i-mdi-calendar"
+        link="/calendar"
+      />
+
+      <NavsBottomNavLink
+        title="نبذة"
+        icon="i-mdi-help-circle"
+        link="/about"
+      />
+
+      
+
     </div>
   </div>
 </template>
