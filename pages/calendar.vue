f3c0c5add2067c043203c3d514d5621313af5bd2
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-10-16 23:46:00 +0300

    fix: calendar page centering

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex-1 bg-secondary-violet text-white ">
+  <div class="flex-1 bg-secondary-violet text-white flex flex-col ">
     <NavsTop title="الروزنامة" description="قم بتحميل روزنامة المبادرة كاملة" additional_classes="bg-secondary-violet" />
 
-    <section id="calendar" class="py-6 flex-1">
+    <section id="calendar" class="py-6 flex-1 flex h-full flex-col justify-center items-center">
       <UContainer class="">
-        <div class="flex flex-col justify-center items-center space-y-8">
+        <div class=" space-y-8">
           <div>الشي الاول</div>
           <div>الشي الثاني</div>
           <div>الشي الثالث</div>
