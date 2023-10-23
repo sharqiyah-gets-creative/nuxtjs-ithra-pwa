6bfc82ef2f3bd6ade928248036dfeb20b72e44b8
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-10-23 23:36:23 +0300

    Update [id].vue

--- pages/event/[id].vue
+++ pages/event/[id].vue
@@ -46,9 +46,12 @@ import { getAverageReviews, getTopReviewsComments } from '~/composables/useEvent
     // check if eventInfo.value has reviews
     // if yes, check if current user has a review, it should be like (eventInfo.value.reviews[user.uid]) then set it to current_user_review
     if(eventInfo.value.reviews) {
-        if(eventInfo.value.reviews[user.uid]) {
-            current_user_review.value = eventInfo.value.reviews[user.uid];
+        if(user){
+            if(eventInfo.value.reviews[user.uid]) {
+                current_user_review.value = eventInfo.value.reviews[user.uid];
+            }
         }
+        
     }
     
     if(current_user_review.value) {
